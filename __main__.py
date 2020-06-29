f255c7a5cfc7f1403a6ae8c0e619eb495544fe1b
Jon Fairbanks <fairplay89@gmail.com>
2020-06-28 22:34:02 -0700

    Capture Ctrl + C

--- __main__.py
+++ __main__.py
@@ -1,8 +1,12 @@
 import os
+import sys
 from multiprocessing import Process
 from app import webstreaming
 if __name__ == '__main__':
-    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;0" # Ensure OpenCV uses UDP streams
-    p = Process(target=webstreaming)
-    p.start()
-    p.join()
+    try:
+        os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;0" # Ensure OpenCV uses UDP streams
+        p = Process(target=webstreaming)
+        p.start()
+        p.join()
+    except KeyboardInterrupt:
+        sys.exit()
